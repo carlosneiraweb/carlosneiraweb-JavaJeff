9b357d44a8b475f7499cfede1e2baaa8cb12e507
carlos <carlos@server>
2020-10-06 22:51:36 +0200

    base

---
+++ src/Arrays/Arrays.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arrays;
+
+/**
+ *
+ * @author carlos
+ */
+public class Arrays {
+
+    public Arrays() {
+    }
+    
+    
+//String [] coche;
+public String[] modelo = {"seat", "bmw", "opel"};
+
+public String[] cambiarDatos(String item, int posicion){
+    
+            modelo[posicion] = item;
+            
+            
+    return modelo;
+    
+}   
+    
+    
+    
+    
+//fin Arrays    
+    
+}
